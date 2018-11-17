8bcabfa79512fe0d364366f62ac501c204874672
Nathaniel Nunes <manutdkid77@gmail.com>
2018-11-17 19:39:22 +0530

    Added class to hold commonly used strings

---
+++ Identica/Helpers/StringConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identica.Helpers
+{
+    public static class StringConstants
+    {
+        public static string UserExists = "User already exists";
+
+        public static string UserNotFound = "User does not exist";
+
+        public static string WrongPwdEmail = "You have entered an incorrect Email or Password";
+
+        public static string ProcessingError = "An error occurred when processing the request";
+    }
+}
