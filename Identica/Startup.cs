917f34fcf8ab4f2d6993477b97781d7b77f0e22e
Nathaniel Nunes <manutdkid77@gmail.com>
2018-11-17 12:08:48 +0530

    Corrected IConfiguration to inject it in the constructor and configured Identity Service and added it to pipeline

--- Identica/Startup.cs
+++ Identica/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Identica.Entities;
+using Identica.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,25 +15,37 @@ namespace Identica
         public static IConfiguration _configuration { get; private set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+
+        public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
 
             var strConnectionString = _configuration["connectionString:IdenticaDBConnectionString"];
 
-            services.AddDbContext<IdenticaDBContext>(options => 
-            {
-                options.UseSqlServer(strConnectionString);
-            });
+            services.AddMvc();
+
+            services.AddDbContext<IdenticaDBContext>(options => options.UseSqlServer(strConnectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<IdenticaDBContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
 
             app.Run(async (context) =>
             {
